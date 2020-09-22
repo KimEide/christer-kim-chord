1f095266834c5699cfce03a78695072c7e4e9209
Kim Grønning Eide <kei058@uit.no>
2020-09-22 17:46:13 +0200

    hashing

---
+++ example_code/runkim.sh
@@ -0,0 +1,5 @@
+python3 kim.py -p 8002 localhost:8006 localhost:8012 & 
+python3 kim.py -p 8006 localhost:8008 localhost:8002 & 
+python3 kim.py -p 8008 localhost:8010 localhost:8006 & 
+python3 kim.py -p 8010 localhost:8012 localhost:8008 &
+python3 kim.py -p 8012 localhost:8002 localhost:8010
