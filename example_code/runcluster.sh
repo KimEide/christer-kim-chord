bfdb453e6bdf5eeb682b77490419792daee2c383
Christer <christer.opda@gmail.com>
2020-09-23 18:51:46 +0200

    la til christer.py i runcluster, comenter ut om du ska kjøre kim

--- example_code/runcluster.sh
+++ example_code/runcluster.sh
@@ -9,10 +9,16 @@ tail -n +2 $"hostfile-sorted.txt" > $"FILE.tmp" && mv $"FILE.tmp" $"hostfile-sor
 node5=$(head -n +1 $"hostfile-sorted.txt")
 tail -n +2 $"hostfile-sorted.txt" > $"FILE.tmp" && mv $"FILE.tmp" $"hostfile-sorted.txt"
 
-ssh $node1 python3 $PWD/kim.py -p 64500 $node2:64500 $node5:64500 &
-ssh $node2 python3 $PWD/kim.py -p 64500 $node3:64500 $node1:64500 &
-ssh $node3 python3 $PWD/kim.py -p 64500 $node4:64500 $node2:64500 &
-ssh $node4 python3 $PWD/kim.py -p 64500 $node5:64500 $node3:64500 &
-ssh $node5 python3 $PWD/kim.py -p 64500 $node1:64500 $node4:64500
+# ssh $node1 python3 $PWD/kim.py -p 64500 $node2:64500 $node5:64500 &
+# ssh $node2 python3 $PWD/kim.py -p 64500 $node3:64500 $node1:64500 &
+# ssh $node3 python3 $PWD/kim.py -p 64500 $node4:64500 $node2:64500 &
+# ssh $node4 python3 $PWD/kim.py -p 64500 $node5:64500 $node3:64500 &
+# ssh $node5 python3 $PWD/kim.py -p 64500 $node1:64500 $node4:64500
+
+ssh $node1 python3 $PWD/christer.py -p 64500 $node2:64500 $node5:64500 &
+ssh $node2 python3 $PWD/christer.py -p 64500 $node3:64500 $node1:64500 &
+ssh $node3 python3 $PWD/christer.py -p 64500 $node4:64500 $node2:64500 &
+ssh $node4 python3 $PWD/christer.py -p 64500 $node5:64500 $node3:64500 &
+ssh $node5 python3 $PWD/christer.py -p 64500 $node1:64500 $node4:64500
 
 $node1 > $"constant.txt" 
