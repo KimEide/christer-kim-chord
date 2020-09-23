9c71d575f820a71aaa5b17c1769d2758b2c7114c
Christer <christer.opda@gmail.com>
2020-09-23 18:50:07 +0200

    adda client til makefile

--- example_code/runjoin.sh
+++ example_code/runjoin.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 node=$(head -n +1 $"hostfile-sorted.txt")
+constant=$(head -n +1 $"constant.txt")
 
-echo ssh -f $node '"'python3 "$"PWD/christer-kim/example_code/christer.py -c True -j $node:8000'"'
+echo ssh -f $node '"'python3 "$"PWD/christer-kim/example_code/christer.py -c True -j $constant:8000'"'
 
 tail -n +2 $"hostfile-sorted.txt" > $"FILE.tmp" && mv $"FILE.tmp" $"hostfile-sorted.txt"
