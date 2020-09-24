571a942fd7c9d3e409667045386ac7f84d972b4f
christer <christer.opda@gmail.com>
2020-09-24 16:20:10 +0200

    yolo

--- example_code/runjoin.sh
+++ example_code/runjoin.sh
@@ -5,6 +5,6 @@ c_file="constant.txt"
 node=$(head -n +1 "$filename") 
 constant=$(head -n +1 "$c_file")
 
-ssh -f "$node python3 $PWD/christer.py -c True -j $constant:5231"
+ssh -f $node "python3 $PWD/christer.py -p 5231 -c True -j $constant:5231"
 
 tail -n +2 $"hostfile-sorted.txt" > $"FILE.tmp" && mv $"FILE.tmp" $"hostfile-sorted.txt"
