8ef5c42a52a224a79d62823894bb655d163651c3
Christer <christer.opda@gmail.com>
2020-09-29 12:39:24 +0200

    ugh

--- code/run16.sh
+++ code/run16.sh
@@ -6,7 +6,7 @@ python3 genhostfile.py $m
 
 filename="hostfile-random.txt"
 file_to_write="constant.txt"
-port=62310
+port=5231
 
 node1=$(head -n +1 "$filename") 
 echo $node1 > "$file_to_write" 
--- code/runclient.sh
+++ code/runclient.sh
@@ -1,4 +1,4 @@
 c_file="constant.txt"
 constant=$(head -n +1 $"constant.txt") 
 
-python3 client.py $constant:62310
+python3 client.py $constant:5231
